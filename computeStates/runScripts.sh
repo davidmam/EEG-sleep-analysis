f83ab9f55ad2def50693600deaca63c2e9419ce3
Vassia Katsageorgiou <kats.vassia@gmail.com>
2017-12-15 13:56:13 +0100

    need to complete analysis script

--- computeStates/runScripts.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-BASE_DIR="/run/media/vkatsageorgiou/data/mouseSleepAnalysis/journalpaper_dsetsAsWebpage/experiment1/mcRBManalysis/"
-
-modelName="ws_fac11_cov11_mean10.mat"
-
-python2 inferStates.py -f ${BASE_DIR} -done "True" -m ${modelName}
